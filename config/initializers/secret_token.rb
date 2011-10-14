0d6bafc37b6ebed3311cd11070f9dd97acf9bc7c
Michaël Maillot <michael.maillot@gmail.com>
2011-10-14 10:23:56 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maquette::Application.config.secret_token = '0f510c329b952c838e60fb344bbba551e5d207ea4c93f4005c3f95820b2576749c6e45e4bb829ae1ee5f3980b4423646efa0eca366eeb6dc2f0bb01eeecb070b'
